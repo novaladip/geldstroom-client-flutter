f914c07ed3c381356a3b26d4152aa165ef8b6d05
A. Noval Adip <a.novaladip@yahoo.com>
2020-12-07 05:24:57 +0700

    [widgets] added CustomTextFormField

---
+++ lib/shared/widget/custom_text_form_field/custom_text_form_field.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../common/config/config.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    Key key,
+    this.validator,
+    @required this.labelText,
+    this.enabled = true,
+    this.focusNode,
+    this.keyboardType = TextInputType.text,
+    this.textInputAction = TextInputAction.unspecified,
+    this.onFieldSubmitted,
+    this.controller,
+    this.obscureText = false,
+    this.onSaved,
+    this.errorText,
+    this.helperText,
+    this.onTap,
+    this.initialValue,
+    this.onChanged,
+    this.inputFormatters = const [],
+  }) : super(key: key);
+
+  final String Function(String) validator;
+  final void Function() onFieldSubmitted;
+  final String labelText;
+  final bool enabled;
+  final FocusNode focusNode;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
+  final TextEditingController controller;
+  final bool obscureText;
+  final String errorText;
+  final Function(String) onSaved;
+  final String helperText;
+  final void Function() onTap;
+  final String initialValue;
+  final Function(String) onChanged;
+  final List<TextInputFormatter> inputFormatters;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).primaryColor;
+
+    final inputDecoration = InputDecoration(
+      focusColor: color,
+      contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      labelStyle: TextStyle(
+        fontFamily: AppStyles.fontFamilyTitle,
+        color: AppStyles.textGray,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.white,
+          width: 0.8,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(8.w)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 1,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(8.w)),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8.w)),
+      ),
+      filled: true,
+      labelText: labelText,
+      enabled: enabled,
+      errorText: errorText,
+      helperText: helperText,
+    );
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 13.h),
+      child: TextFormField(
+        autocorrect: false,
+        initialValue: initialValue,
+        style: TextStyle(
+          fontFamily: AppStyles.fontFamilyTitle,
+          color: AppStyles.textWhite,
+        ),
+        enabled: enabled,
+        onTap: onTap,
+        controller: controller,
+        obscureText: obscureText,
+        onSaved: onSaved,
+        focusNode: focusNode,
+        validator: validator,
+        keyboardType: keyboardType,
+        textInputAction: textInputAction,
+        onChanged: onChanged,
+        onFieldSubmitted: (_) => onFieldSubmitted(),
+        inputFormatters: inputFormatters,
+        decoration: inputDecoration,
+        cursorColor: AppStyles.primaryColor,
+        keyboardAppearance: Brightness.dark,
+      ),
+    );
+  }
+}
---
+++ lib/shared/widget/custom_text_form_field/custom_text_form_field_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:geldstroom/test_helper.dart';
+
+import 'custom_text_form_field.dart';
+
+class Home extends StatelessWidget {
+  final emailController = TextEditingController();
+
+  Home({
+    Key key,
+    @required this.labelText,
+    @required this.textInputKey,
+    @required this.onFieldSubmitted,
+  }) : super(key: key);
+
+  final String labelText;
+  final Key textInputKey;
+  final VoidCallback onFieldSubmitted;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomTextFormField(
+        labelText: labelText,
+        controller: emailController,
+        key: textInputKey,
+        onFieldSubmitted: onFieldSubmitted,
+      ),
+    );
+  }
+}
+
+void main() {
+  testWidgets('TextInput, should render correctly', (tester) async {
+    var isSubmitted = false;
+    final textInputKey = UniqueKey();
+    final labelText = 'Email';
+
+    final expectedInputValue = 'some@email.com';
+    final subject = buildTestableWidget(Home(
+      labelText: labelText,
+      textInputKey: textInputKey,
+      onFieldSubmitted: () => isSubmitted = true,
+    ));
+
+    await tester.pumpWidget(subject);
+
+    expect(isSubmitted, false);
+    expect(find.text(labelText), findsOneWidget);
+    expect(find.byKey(textInputKey), findsOneWidget);
+    expect(find.text(expectedInputValue), findsNothing);
+
+    // simulate user input
+    await tester.enterText(find.byKey(textInputKey), expectedInputValue);
+    // simulate sending action keyboard event
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pumpAndSettle();
+    expect(find.text(expectedInputValue), findsOneWidget);
+    expect(isSubmitted, true);
+  });
+}
