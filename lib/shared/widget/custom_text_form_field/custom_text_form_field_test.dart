f880f8d7a8bb3dfb8f7b29ba225baf54fb92ea5c
A. Noval Adip <a.novaladip@yahoo.com>
2020-12-08 12:30:36 +0700

    [CustomTextFormField] fix lint

--- lib/shared/widget/custom_text_form_field/custom_text_form_field_test.dart
+++ lib/shared/widget/custom_text_form_field/custom_text_form_field_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:geldstroom/test_helper.dart';
 
+import '../../../test_helper.dart';
 import 'custom_text_form_field.dart';
 
 class Home extends StatelessWidget {
