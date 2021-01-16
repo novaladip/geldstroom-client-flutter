2d971c8bf950633fa2f73ed1a2d2a0a0f4800952
A. Noval Adip <a.novaladip@yahoo.com>
2021-01-16 15:11:44 +0700

    [utils] added AmountFormatter

--- lib/shared/common/utils/format_currency/format_currency.dart
+++ lib/shared/common/utils/format_currency/format_currency.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart' as intl;
 
 class FormatCurrency {
@@ -6,4 +7,30 @@ class FormatCurrency {
         decimalDigits: 0,
         symbol: 'IDR',
       ).format(number);
+}
+
+class AmountFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    var newText = newValue.text;
+    if (newText.isNotEmpty) {
+      newText = _formatCurrency(newValue.text);
+    }
+    var selectionIndex = newText.length;
+
+    return TextEditingValue(
+      text: newText,
+      selection: TextSelection.collapsed(offset: selectionIndex),
+    );
+  }
+
+  String _formatCurrency(String value) {
+    final num = int.parse(value);
+    return intl.NumberFormat.currency(
+      locale: 'IN',
+      decimalDigits: 0,
+      symbol: '',
+    ).format(num);
+  }
 }
--- test/shared/common/utils/format_currency/format_currency_test.dart
+++ test/shared/common/utils/format_currency/format_currency_test.dart
@@ -1,11 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geldstroom/shared/common/utils/utils.dart';
+
+import '../../../../test_helper.dart';
 
 void main() {
   group('FormatCurrency', () {
     test('toIDR', () {
       final number = 10000;
       expect(FormatCurrency.toIDR(number), 'IDR10.000');
+    });
+  });
+
+  group('AmountFormatter', () {
+    testWidgets('shoud', (tester) async {
+      final key = UniqueKey();
+      final amountFormatter = AmountFormatter();
+      final subject = buildTestableWidget(
+        TextFormField(
+          key: key,
+          inputFormatters: [amountFormatter],
+        ),
+      );
+      await tester.pumpWidget(subject);
+      await tester.enterText(find.byKey(key), 's3x2000');
+      await tester.enterText(find.byKey(key), '32000');
+      await tester.pumpAndSettle();
+      expect(find.text('32.000'), findsOneWidget);
     });
   });
 }
