8711223734ae0f0a92b5f73e32ad9804ffc8fad9
A. Noval Adip <a.novaladip@yahoo.com>
2021-01-26 13:25:11 +0700

    [widget] added ErrorMessageRetry

--- lib/shared/widget/error_message/error_message.dart
+++ lib/shared/widget/error_message/error_message.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geldstroom/shared/widget/widget.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 import '../../../gen/assets.gen.dart';
@@ -8,7 +9,7 @@ import '../../common/config/config.dart';
 class ErrorMessage extends StatelessWidget {
   const ErrorMessage({
     Key key,
-    this.message,
+    @required this.message,
   }) : super(key: key);
 
   final String message;
@@ -22,6 +23,36 @@ class ErrorMessage extends StatelessWidget {
           .fontWeight(FontWeight.w500)
           .fontFamily(AppStyles.fontFamilyTitle)
           .textAlignment(TextAlign.center),
+    ].toColumn().padding(top: 0.13.sh);
+  }
+}
+
+class ErrorMessageRetry extends StatelessWidget {
+  static const retryText = 'Retry';
+
+  const ErrorMessageRetry({
+    Key key,
+    this.message,
+    @required this.onRetry,
+  }) : super(key: key);
+
+  final String message;
+  final VoidCallback onRetry;
+
+  @override
+  Widget build(BuildContext context) {
+    return <Widget>[
+      Assets.images.fetchError.svg(height: .3.sh).padding(bottom: 20.h),
+      Text(message)
+          .fontSize(32.sp)
+          .fontWeight(FontWeight.w500)
+          .fontFamily(AppStyles.fontFamilyTitle)
+          .textAlignment(TextAlign.center),
+      SizedBox(height: 20.h),
+      MainButton(
+        title: retryText,
+        onTap: onRetry,
+      ),
     ].toColumn().padding(top: 0.13.sh);
   }
 }
--- test/shared/widget/error_message/error_message_test.dart
+++ test/shared/widget/error_message/error_message_test.dart
@@ -6,9 +6,9 @@ import 'package:geldstroom/shared/widget/widget.dart';
 import '../../../test_helper.dart';
 
 void main() {
+  final message = 'Ops something went wrong';
   group('ErrorMessage', () {
     Widget subject;
-    final message = 'Ops something went wrong';
 
     setUp(() {
       subject = buildTestableWidget(
@@ -21,6 +21,37 @@ void main() {
         await tester.pumpWidget(subject);
         expect(find.byType(SvgPicture), findsOneWidget);
         expect(find.text(message), findsOneWidget);
+      });
+    });
+  });
+
+  group('ErrorMessageRetry', () {
+    Widget subject;
+    var retry = false;
+    void onRetry() => retry = true;
+
+    setUp(() {
+      subject = buildTestableWidget(
+        ErrorMessageRetry(
+          message: message,
+          onRetry: onRetry,
+        ),
+      );
+    });
+
+    group('renders', () {
+      testWidgets('correctly', (tester) async {
+        await tester.pumpWidget(subject);
+        expect(find.text(message), findsOneWidget);
+        expect(find.text(ErrorMessageRetry.retryText), findsOneWidget);
+      });
+    });
+
+    group('calls', () {
+      testWidgets('onRetry', (tester) async {
+        await tester.pumpWidget(subject);
+        await tester.tap(find.text(ErrorMessageRetry.retryText).hitTestable());
+        expect(retry, true);
       });
     });
   });
