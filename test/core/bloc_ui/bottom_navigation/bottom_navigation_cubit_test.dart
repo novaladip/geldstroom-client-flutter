e211ede25f12afc595ef5fc620a0efad4db5e1d5
A. Noval Adip <a.novaladip@yahoo.com>
2021-02-12 06:22:55 +0700

    [BottomNavigationCubit] fix test

--- test/core/bloc_ui/bottom_navigation/bottom_navigation_cubit_test.dart
+++ test/core/bloc_ui/bottom_navigation/bottom_navigation_cubit_test.dart
@@ -34,19 +34,25 @@ void main() {
     blocTest<BottomNavigationCubit, BottomNavigationState>(
       'changeSelectedIndex',
       build: () => subject,
-      act: (cubit) => cubit
-        ..changeSelectedIndex(1)
-        ..changeSelectedIndex(2)
-        ..changeSelectedIndex(1),
-      expect: [BottomNavigationState(selectedIndex: 1)],
+      act: (cubit) => cubit.changeSelectedIndex(1),
+      expect: [
+        BottomNavigationState(selectedIndex: 1),
+      ],
     );
 
     blocTest<BottomNavigationCubit, BottomNavigationState>(
       'call balanceReportCubit.refresh when ReportPage is selected',
       build: () => subject,
-      act: (cubit) => cubit.changeSelectedIndex(1),
+      act: (cubit) => cubit
+        ..changeSelectedIndex(1)
+        ..changeSelectedIndex(2)
+        ..changeSelectedIndex(1),
       wait: Duration(seconds: 1),
-      expect: [BottomNavigationState(selectedIndex: 1)],
+      expect: [
+        BottomNavigationState(selectedIndex: 1),
+        BottomNavigationState(selectedIndex: 2),
+        BottomNavigationState(selectedIndex: 1),
+      ],
       verify: (_) {
         verify(balanceReportCubit.refresh()).called(1);
       },
