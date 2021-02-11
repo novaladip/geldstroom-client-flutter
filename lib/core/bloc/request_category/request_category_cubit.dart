c2e4a7ef013d0be512e5e3203dcd333dc33053da
A. Noval Adip <a.novaladip@yahoo.com>
2021-02-12 04:08:33 +0700

    [RequestCategoryCubit] added RequestCategoryDeleteCubit listener

--- lib/core/bloc/request_category/request_category_cubit.dart
+++ lib/core/bloc/request_category/request_category_cubit.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../bloc_base/bloc_base.dart';
 import '../../network/model/model.dart';
 import '../../network/service/service.dart';
 import '../bloc.dart';
@@ -12,19 +13,33 @@ part 'request_category_state.dart';
 @lazySingleton
 class RequestCategoryCubit extends Cubit<RequestCategoryState> {
   RequestCategoryCubit(
-    this._service,
-    this._authCubit,
-  ) : super(RequestCategoryState.initial()) {
-    _authCubit.listen((state) {
-      // clear state when auth state is unauthenticated
-      if (state is AuthStateUnauthenticated) {
-        emit(RequestCategoryState.initial());
-      }
-    });
+    this._service, {
+    @required AuthCubit authCubit,
+    @required RequestCategoryDeleteCubit requestCategoryDeleteCubit,
+  }) : super(RequestCategoryState.initial()) {
+    authCubit.listen(_authCubitListener);
+    requestCategoryDeleteCubit.listen(_deleteListener);
   }
 
+  void _authCubitListener(AuthState state) {
+    // clear state when auth state is unauthenticated
+    if (state is AuthStateUnauthenticated) {
+      clear();
+    }
+  }
+
+  // listen for RequestCategoryDeleteCubit
+  void _deleteListener(DeleteState state) {
+    // call _onDelete everytime there is a new id in onDeleteSuccessIds
+    if (state.shouldListenDeleteSuccess(_prevDeleteState)) {
+      _onDelete(state.onDeleteSuccessIds[0]);
+    }
+    // update _prevDeleteState value
+    _prevDeleteState = state;
+  }
+
+  var _prevDeleteState = DeleteState.initial();
   final IRequestCategoryService _service;
-  final AuthCubit _authCubit;
 
   Future<void> fetch() async {
     emit(state.copyWith(status: FetchStatus.loadInProgress()));
@@ -42,6 +57,14 @@ class RequestCategoryCubit extends Cubit<RequestCategoryState> {
         );
       },
     );
+  }
+
+  void _onDelete(String requestCategoryId) {
+    final newState = state.copyWith(
+      data: state.data.where((data) => data.id != requestCategoryId).toList(),
+    );
+
+    emit(newState);
   }
 
   void clear() {
--- test/core/bloc/request_category/request_category_cubit_test.dart
+++ test/core/bloc/request_category/request_category_cubit_test.dart
@@ -3,6 +3,8 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geldstroom/core/bloc/auth/auth_cubit.dart';
 import 'package:geldstroom/core/bloc/request_category/request_category_cubit.dart';
+import 'package:geldstroom/core/bloc/request_category_delete/request_category_delete_cubit.dart';
+import 'package:geldstroom/core/bloc_base/delete/delete_cubit.dart';
 import 'package:geldstroom/core/network/network.dart';
 import 'package:mockito/mockito.dart';
 
@@ -13,11 +15,15 @@ class MockIRequestCategoryService extends Mock
 
 class MockAuthCubit extends MockBloc<AuthState> implements AuthCubit {}
 
+class MockRequestCategoryDeleteCubit extends MockBloc<DeleteState>
+    implements RequestCategoryDeleteCubit {}
+
 void main() {
   group('RequestCategoryCubit', () {
     IRequestCategoryService service;
     RequestCategoryCubit subject;
     AuthCubit authCubit;
+    RequestCategoryDeleteCubit requestCategoryDeleteCubit;
 
     final data = RequestCategoryJson.list
         .map((json) => RequestCategory.fromJson(json))
@@ -27,7 +33,12 @@ void main() {
     setUp(() {
       service = MockIRequestCategoryService();
       authCubit = MockAuthCubit();
-      subject = RequestCategoryCubit(service, authCubit);
+      requestCategoryDeleteCubit = MockRequestCategoryDeleteCubit();
+      subject = RequestCategoryCubit(
+        service,
+        authCubit: authCubit,
+        requestCategoryDeleteCubit: requestCategoryDeleteCubit,
+      );
     });
 
     tearDown(() {
@@ -80,9 +91,44 @@ void main() {
         'call clear when state is AuthStateUnauthenticated',
         build: () {
           whenListen(authCubit, Stream.value(AuthState.unauthenticated()));
-          return RequestCategoryCubit(service, authCubit);
+          return RequestCategoryCubit(
+            service,
+            authCubit: authCubit,
+            requestCategoryDeleteCubit: requestCategoryDeleteCubit,
+          );
         },
         expect: [RequestCategoryState.initial()],
+      );
+    });
+
+    group('listen RequestCategoryDeleteCubit', () {
+      final initialState = RequestCategoryState(
+        data: data,
+        status: FetchStatus.loadSuccess(),
+      );
+      final stateAfterDelete = initialState.copyWith(
+        data: data.where((item) => item.id != data[0].id).toList(),
+      );
+      blocTest<RequestCategoryCubit, RequestCategoryState>(
+        'call _onDelete when state is AuthStateUnauthenticated',
+        seed: initialState,
+        build: () {
+          final deleteState = DeleteState(
+            onDeleteFailureIds: [],
+            onDeleteProgressIds: [],
+            onDeleteSuccessIds: [data[0].id],
+          );
+          whenListen(
+            requestCategoryDeleteCubit,
+            Stream.value(deleteState),
+          );
+          return RequestCategoryCubit(
+            service,
+            authCubit: authCubit,
+            requestCategoryDeleteCubit: requestCategoryDeleteCubit,
+          );
+        },
+        expect: [stateAfterDelete],
       );
     });
   });
